dab32f50cdccdd7ea4c721340cddccc9c602b950
Sy Rashid <sy.rashid.11@gmail.com>
2021-03-07 07:26:02 -0500

    FEAT: Added ability for user to upload new suvey with photo

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,6 +5,19 @@ class SurveysController < ApplicationController
   end
 
   def create
+    @survey = Survey.new(survey_params)
+    @survey.user = current_user
+    authorize @survey
+    if @survey.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def survey_params
+    params.require(:survey).permit(:style, :photo)
   end
 end
