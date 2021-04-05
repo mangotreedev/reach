b204a56edd7bd771215186b897df1525080b9e4c
Sy Rashid <sy.rashid.11@gmail.com>
2021-04-04 22:37:46 -0400

    FIX: Modified authentication behaviour

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,6 @@
 class ResultsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
   def show
     @survey = Survey.includes(:choices).find(params[:survey_id])
     authorize @survey
