55bf5aa2fa594b323962e29557d72815d7a278ea
Sy Rashid <sy.rashid.11@gmail.com>
2021-04-04 21:23:04 -0400

    REFACTOR: Added small includes to avoid n+1 query

--- app/services/random_survey_selector.rb
+++ app/services/random_survey_selector.rb
@@ -2,7 +2,7 @@ class RandomSurveySelector
   class << self
     def call(cookie)
       voted_surveys = Survey.joins(choices: :votes).where(choices: { votes: {cookie: cookie}})
-      Survey.published.approved.where.not(id: voted_surveys.map(&:id)).sample
+      Survey.includes([:photo_attachment, :choices]).published.approved.where.not(id: voted_surveys.map(&:id)).sample
     end
   end
 end
