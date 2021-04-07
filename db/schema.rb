de07bd3cde4ac54949c75ea5e611a9240259c7ea
Sy Rashid <sy.rashid.11@gmail.com>
2021-04-06 21:02:34 -0400

    FIX: Made admin attribute default to false

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2021_04_07_010125) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "agree_terms"
     t.string "username"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
