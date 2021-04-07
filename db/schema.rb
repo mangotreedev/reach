7a2976abc7f47e471fa20baae74e232d65ec8197
Sy Rashid <sy.rashid.11@gmail.com>
2021-04-06 21:01:44 -0400

    ARCH: Added admin attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_152034) do
+ActiveRecord::Schema.define(version: 2021_04_07_010125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_04_04_152034) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "agree_terms"
     t.string "username"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
