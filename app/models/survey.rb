7a1fb1cdb554886957469ef30903efb2f31ebf42
Sy Rashid <sy.rashid.11@gmail.com>
2021-04-06 22:31:47 -0400

    REFACTOR: Made most and least filters only select elements that have controversy

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -37,7 +37,7 @@ class Survey < ApplicationRecord
     # Controversy is calculated based off two factors; vote count threshold and vote standard deviation
 
     # Comment this back in when you have a sizable amount of surveys and votes
-    return 0 if votes.count < 3
+    return false if votes.count < 2
     variance = vote_counts.reduce(0) { |variance, x| variance += (x - mean) ** 2 }
     sigma = Math.sqrt(variance/(vote_counts.size-1))
 
