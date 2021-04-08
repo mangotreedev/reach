14e8c94af767769ac26ae5b844af5b5d560282ee
Sy Rashid <sy.rashid.11@gmail.com>
2021-04-07 20:54:53 -0400

    FEAT: Added tool tips to edit and results page

--- app/javascript/plugins/init_tippy.js
+++ app/javascript/plugins/init_tippy.js
@@ -4,6 +4,12 @@ const initTippy = () => {
   tippy(".delete-btn", {
     content: "Delete Photo",
   });
+  tippy(".edit-btn", {
+    content: "Add Choices & Publish",
+  });
+  tippy(".results-btn", {
+    content: "View Results",
+  });
 }
 
 export default initTippy;
