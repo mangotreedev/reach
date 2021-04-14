cefdbf4b95c02bb6c6a34776e56ac1bf994bc690
Sy Rashid <sy.rashid.11@gmail.com>
2021-04-14 19:27:27 -0400

    FEAT: Add background transition to navbar

--- app/javascript/components/init_navbar_scroll.js
+++ app/javascript/components/init_navbar_scroll.js
@@ -1,3 +1,32 @@
 const initNavbarScroll = () => {
-  const navbar = document.querySelector('.js-nav-scroll')
-}
+  const navbar = document.querySelector(".js-nav-scroll");
+  if (navbar) {
+    function debounce(func, wait = 10, immediate = true) {
+      var timeout;
+      return function () {
+        var context = this,
+          args = arguments;
+        var later = function () {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+
+    const navbarCheck = () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add("bg-white");
+      } else {
+        navbar.classList.remove("bg-white");
+      }
+    }
+
+    window.addEventListener("scroll", debounce(navbarCheck));
+  }
+};
+
+export default initNavbarScroll;
