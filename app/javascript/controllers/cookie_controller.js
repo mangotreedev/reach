8a446b7eddc3e25e5a202c23760000e96a103178
Sy Rashid <sy.rashid.11@gmail.com>
2021-04-28 01:23:13 -0400

    FEAT: Add reach cookie monster cookie

--- app/javascript/controllers/cookie_controller.js
+++ app/javascript/controllers/cookie_controller.js
@@ -1,18 +1,27 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["popup"];
+  static targets = ["popup", "button"];
 
-  display() {
-    this.popupTarget.classList.add("is-visible");
+  _getCookie(name) {
+    let result = document.cookie.match("(^|[^;]+)\\s*" + name + "\\s*=\\s*([^;]+)")
+    return result ? result.pop() : ""
   }
 
-  hide() {
+	_setCookie(name, value) {
+      document.cookie = `${name}=${value}`
+  }
+
+  _hide() {
     this.popupTarget.classList.remove("is-visible");
   }
 
   agree() {
+    this._setCookie("reach_cookie_monster", "me_love_cookie")
+    if (event.target === this.popupTarget || this.buttonTarget) this._hide();
+  }
 
-    if (event.target === this.popupTarget) this.hide();
+  disagree() {
+    this._hide();
   }
 }
