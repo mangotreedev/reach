d182bf3261b4a0b5ddba0eb6c05105c81f03f64f
Sy Rashid <sy.rashid.11@gmail.com>
2021-12-22 13:53:00 +0000

    FEAT: Add delegation to survey for vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,12 @@
 class Vote < ApplicationRecord
   belongs_to :choice
+  delegate :survey, to: :choice, allow_nil: true
+
+  after_create :broadcast_results
+
+  private
+
+  def broadcast_results
+
+  end
 end
